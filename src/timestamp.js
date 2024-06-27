ece9c3507c25e098add7092bd9c5242ba2d232b1
Himanshu Kumar Modi <114941090+himanshumodi2001@users.noreply.github.com>
2024-06-27 15:16:23 +0530

    Update timestamp.js

--- src/timestamp.js
+++ src/timestamp.js
@@ -1,13 +1,12 @@
-const timestamp = Date.now();
-const dateObject = new Date(timestamp);
-const date = dateObject.getDate();
-const month = dateObject.getMonth();
-const year = dateObject.getFullYear();
-const hour = dateObject.getHours();
-const minute = dateObject.getMinutes();
-const second = dateObject.getSeconds();
-const time = date+""+month+""+year+""+hour+""+minute+""+second;
-const a = {
+var d = new Date();
+var year = d.getFullYear();
+var date = d.getDate();
+var month = d.getMonth();
+var hour = d.getHours();
+var minute = d.getMinutes();
+var second = d.getSeconds();
+var time = date+""+month+""+year+""+hour+""+minute+""+second;
+var a = {
     TIME: time
 }
 export {a}
