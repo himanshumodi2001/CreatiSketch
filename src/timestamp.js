71f5430337679bd09429bd44ed07d35a5f97a728
Himanshu Kumar Modi <114941090+himanshumodi2001@users.noreply.github.com>
2024-06-27 15:20:23 +0530

    Update timestamp.js

--- src/timestamp.js
+++ src/timestamp.js
@@ -5,6 +5,17 @@ var month = d.getMonth();
 var hour = d.getHours();
 var minute = d.getMinutes();
 var second = d.getSeconds();
+
+if(hour < 10){
+    hour = "0" + hours;   
+    }
+if(minute < 10){
+    minute = "0" + minutes;   
+}
+if(second < 10){
+    second = "0" + seconds;   
+}
+
 var time = date+""+month+""+year+""+hour+""+minute+""+second;
 var a = {
     TIME: time
